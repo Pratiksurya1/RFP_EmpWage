20792d0844e6451f713dbc91b051e02c5896bfa4
Pratiksurya1 <suryawanshipratik356@gmail.com>
2022-01-12 23:05:14 +0530

    Add uc3 program

---
+++ Uc3.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RFP_EmpWage
+{
+    internal class Uc3
+    {
+        int isPartTime = 1;
+        int isFullTime = 2;
+        int empRatePerHr = 20;
+        int empWorkHr;
+        int empSalary;
+        public void EmpWage()
+        {
+            Random random = new Random();
+            int workType = random.Next(3);
+            if (isPartTime == workType)
+            {
+                empWorkHr = 4;
+                empSalary = empWorkHr * empRatePerHr;
+            }
+            else if (isFullTime == workType)
+            {
+                empWorkHr = 8;
+                empSalary = empWorkHr * empRatePerHr;
+            }
+            Console.WriteLine("Emp work " + empWorkHr + " salary is " + empSalary);
+        }
+    }
+}
