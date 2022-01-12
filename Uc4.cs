17491cef6ea4dfc8c1a8d7227f2935632de6c947
Pratiksurya1 <suryawanshipratik356@gmail.com>
2022-01-12 23:06:12 +0530

    Add Uc4 program

---
+++ Uc4.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RFP_EmpWage
+{
+    internal class Uc4
+    {
+        int empRatePerHr = 20;
+        int empWorkHr;
+        int empSalary;
+        public void EmpWage()
+        {
+            Random random = new Random();
+            int workType = random.Next(3);
+
+            switch (workType)
+            {
+                case 1:
+                    empWorkHr = 8;
+                    break;
+                case 2:
+                    empWorkHr = 4;
+                    break;
+                default:
+                    empWorkHr = 0;
+                    break;
+            }
+            empSalary = empWorkHr * empRatePerHr;
+            Console.WriteLine("Emp work " + empWorkHr + " salary is " + empSalary);
+        }
+    }
+}
