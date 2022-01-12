a5d26166474832f99e77a2aa4b95b82ac2023c37
Pratiksurya1 <suryawanshipratik356@gmail.com>
2022-01-12 23:02:41 +0530

    Add uc1 program

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RFP_EmpWage
+{
+    internal class Program
+    {
+        public static void Main(String[] args)
+        {
+            Uc6 uc6 = new Uc6();
+            uc6.EmpMonthlyWage();
+
+            Uc5 uc5 = new Uc5();
+            uc5.EmpMonthlyWage();
+
+            Uc4 uc4 = new Uc4();
+            uc4.EmpWage();
+
+            Uc3 uc3 = new Uc3();
+            uc3.EmpWage();
+
+            Uc2 uc2 = new Uc2();
+            uc2.CalculateWage();
+
+            Uc1 uc1 = new Uc1();
+            uc1.Attendance();
+        }
+    }
+}
---
+++ Uc1.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RFP_EmpWage
+{
+    internal class Uc1
+    {
+        public void Attendance()
+        {
+            Random random = new Random();
+            int num = random.Next(2);
+            if (num == 0)
+                Console.WriteLine("emp is presant");
+            else
+                Console.WriteLine("emp is absent ");
+        }
+    }
+}
