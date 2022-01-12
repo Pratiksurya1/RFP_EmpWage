b2236049f800e9804593763d7b5fcd3300a4933e
Pratiksurya1 <suryawanshipratik356@gmail.com>
2022-01-12 23:04:25 +0530

    Add uc2 program

---
+++ Uc2.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RFP_EmpWage
+{
+    internal class Uc2
+    {
+        int empSalary;
+        public void CalculateWage()
+        {
+            Random random = new Random();
+            int isPresent = random.Next(2);
+            if (isPresent == 0)
+            {
+                int empRatePerHr = 20;
+                int empWorkHr = 8;
+                empSalary = empRatePerHr * empWorkHr;
+                Console.WriteLine("emp is presant\n Emp Dialy Wage is " + empSalary);
+            }
+            else
+                Console.WriteLine("emp is absent \n Emp Daily Wage is " + empSalary);
+        }
+    }
+}
